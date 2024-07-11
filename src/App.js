30c8efeccf6f881796b27604a85ee54830f8b433
Devang Thaker <devangt101003@gamil.com>
2024-07-11 21:30:16 +0530

    login register done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <ProductList />
+    <Login/>
   );
 }
 
