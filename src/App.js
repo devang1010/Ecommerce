d6d7e4e7cebe85c0cdc71d7a78a8d99cc8731f93
devangThaker03 <devang.thaker@shipturtle.co>
2025-08-14 22:28:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import ProductList from './pages/ProductList';
-import Product from './pages/Product';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
+import React from "react";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
